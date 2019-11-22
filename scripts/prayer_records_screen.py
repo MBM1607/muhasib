5a1602c3a0fe2f65cfa897f8e670f7d5db0749f4
Muhammad Khan <MuhammadKhan1607@gmail.com>
2019-11-22 20:14:32 +0500

    Fix the bug of records not being showen on screen

--- scripts/prayer_records_screen.py
+++ scripts/prayer_records_screen.py
@@ -15,12 +15,17 @@ class PrayerRecordsScreen(Screen):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 
-		self.bind(on_pre_enter=lambda _: self.create_record_lists())
-		self.bind(on_leave=lambda _: self.record_lists.destroy_lists())
+		self.bind(on_pre_enter=lambda _: self.set_date())
+		self.bind(on_leave=lambda _: self.remove_date())
 
-	def create_record_lists(self):
-		'''Create the prayer record list and set the datepicker to the current day'''
+	def set_date(self):
+		'''Set the datepicker to the current day'''
 		self.datepicker.date = date.today()
+	
+	def remove_date(self):
+		'''Remove the date and the records lists'''
+		self.datepicker.text = ""
+		self.record_lists.destroy_lists()
 
 	def change_date(self):
 		'''Change the date of the prayer record's being showen'''
